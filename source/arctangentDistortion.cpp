025a5e7900eed19679d0527d8b79022c72407bfb
Steven van Esch <steven_ve@live.nl>
2023-08-10 12:45:48 +0200

    added interpolation to setDrive

--- source/arctangentDistortion.cpp
+++ source/arctangentDistortion.cpp
@@ -15,15 +15,15 @@ ArctangentDistortion::~ArctangentDistortion() {
 float ArctangentDistortion::process(float input) {
     float y = 0.5f * M_PI * atanf(drive * input * preGain);
     return y;
-
 }
 
 void ArctangentDistortion::setPreGain(float preGain) {
     this->preGain = preGain;
 }
 
-void ArctangentDistortion::setDrive(float drive) {
-    this->drive = drive;
+void ArctangentDistortion::setDrive(float newDrive) {
+    float value = (drive * alpha) + (previousDrive * alpha);
+    this->drive = value;
 }
 
 void ArctangentDistortion::setMix(float mix) {
--- source/arctangentDistortion.hpp
+++ source/arctangentDistortion.hpp
@@ -13,14 +13,15 @@ public:
     ~ArctangentDistortion();
     float process(float input);
     void setPreGain(float preGain);
-    void setDrive(float threshold);
-    void setMix(float mix);
+    void setDrive(float newDrive);
     void setSampleRate(double sampleRate);
 
 private:
     float preGain{1.0f};
     float drive{0.5f};
     float mix{0.5f};
+    float previousDrive{0.5f};
+    float alpha{0.5f};
     double sampleRate{44100};
 };
 
