65b16c135f8f4bf8467488b3e10e4e6a2473bbf6
Steven van Esch <steven_ve@live.nl>
2023-08-12 14:52:54 +0200

    Added SvEArtworkChorus.cpp/.hpp for GUI assets

---
+++ source/SvEArtworkChorus.hpp
@@ -0,0 +1,20 @@
+/* (Auto-generated binary data file). */
+
+#ifndef BINARY_SVEARTWORKCHORUS_HPP
+#define BINARY_SVEARTWORKCHORUS_HPP
+
+namespace SvEArtworkChorus
+{
+    extern const char* backpanelData;
+    const unsigned int backpanelDataSize = 960000;
+    const unsigned int backpanelWidth    = 400;
+    const unsigned int backpanelHeight   = 600;
+
+    extern const char* knobData;
+    const unsigned int knobDataSize = 21904;
+    const unsigned int knobWidth    = 74;
+    const unsigned int knobHeight   = 74;
+}
+
+#endif // BINARY_SVEARTWORKCHORUS_HPP
+
